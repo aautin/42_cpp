cae2fd7be878e398531e738a5ced324ff9974337
aautin <ezagof@gmail.com>
2024-08-04 17:01:59 +0200

    ignore 01/ex02 executable, its Makefile is done

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 16:28:43 by aautin            #+#    #+#             */
+/*   Updated: 2024/08/04 17:01:52 by aautin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	/* Print addresses */
+	std::cout << "Adresses" << std::endl;
+	std::cout << "string: " << &string << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	/* Print values */
+	std::cout << "Values" << std::endl;
+	std::cout << "string: " << string << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
