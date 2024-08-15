6cf2d474ce09458003cc18b85c82c278b56d6896
aautin <ezagof@gmail.com>
2024-08-15 23:56:08 +0200

    Set the needed vectors (3 pairs, each triangle side)

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -3,21 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
+/*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 08:41:39 by aautin            #+#    #+#             */
-/*   Updated: 2024/08/12 14:45:55 by alexandre        ###   ########.fr       */
+/*   Updated: 2024/08/15 23:54:16 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-bool	bsp( Point const a, Point const b, Point const c, Point const point)
+typedef Point Vector;
+bool	bsp( Point const a, Point const b, Point const c, Point const p)
 {
-	(void) a;
-	(void) b;
-	(void) c;
-	(void) point;
+	/* Calculate a vector from 2 points, A(x, y) B(x', y') : AB = (x' - x, y' - y) */
+	Vector AB(b.getX() - a.getX(), b.getY() - a.getY());
+	Vector AP(p.getX() - a.getX(), p.getY() - a.getY());
+	
+	Vector BC(c.getX() - b.getX(), c.getY() - b.getY());
+	Vector BP(p.getX() - b.getX(), p.getY() - b.getY());
+
+	Vector CA(a.getX() - c.getX(), a.getY() - c.getY());
+	Vector CP(p.getX() - c.getX(), p.getY() - c.getY());
+
+	AB.print("AB");
+
+	/* To be continued (compare vectors all together) ... */
+
 	return true;
 }
