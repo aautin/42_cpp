34c8cdac87d23d9b843006ba9495f58b34456aae
aautin <ezagof@gmail.com>
2024-08-15 23:54:47 +0200

    define getters and a constructor from 2 Fixed class

--- 02/ex03/Point.cpp
+++ 02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 08:10:07 by aautin            #+#    #+#             */
-/*   Updated: 2024/08/15 23:33:17 by aautin           ###   ########.fr       */
+/*   Updated: 2024/08/15 23:50:35 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,25 @@
 Point::Point() : x(0), y(0) {}
 Point::~Point() {}
 Point::Point(float const x, float const y) : x(x), y(y) {}
+Point::Point(Fixed const &x, Fixed const &y) : x(x), y(y) {}
 Point::Point(Point const &operand) : x(operand.x), y(operand.y) {}
-Point	&Point::operator=(Point &operand) { return *this; }
+Point	&Point::operator=(Point &operand)
+{
+	(void) operand;
+	return *this;
+}
 
 /* Others */
 void	Point::print(std::string name) const
 {
 	std::cout << name << " : " << this->x.toFloat() << "x";
 	std::cout << ", " << this->y.toFloat() << "y." << std::endl;
+}
+Fixed	Point::getX() const
+{
+	return Fixed(x);
+}
+Fixed	Point::getY() const
+{
+	return Fixed(y);
 }
--- 02/ex03/Point.hpp
+++ 02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 08:10:29 by aautin            #+#    #+#             */
-/*   Updated: 2024/08/15 22:13:43 by aautin           ###   ########.fr       */
+/*   Updated: 2024/08/15 23:48:21 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,15 @@ class Point
 		/* OCCF */
 		Point();
 		Point(float const x, float const y);
+		Point(Fixed const &x, Fixed const &y);
 		Point(Point const &operand);
 		Point &operator=(Point &operand);
 		~Point();
 
 		/* Others */
 		void	print(std::string name) const;
+		Fixed	getX() const;
+		Fixed	getY() const;
 };
 
 #endif
