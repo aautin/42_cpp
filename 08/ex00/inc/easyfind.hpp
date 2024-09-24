61c4421c0f88836272dd6449c1d46cec257d6899
aautin <ezagof@gmail.com>
2024-09-24 14:49:17 +0200

    use find (algorithm lib) instead of builtin algo

--- 08/ex00/inc/easyfind.hpp
+++ 08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 13:52:51 by aautin            #+#    #+#             */
-/*   Updated: 2024/09/24 14:21:48 by aautin           ###   ########.fr       */
+/*   Updated: 2024/09/24 14:48:51 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
 # include <exception>
 # include <vector>
 
 template <typename T>
-int	easyfind(T const & array, int index) {
-	std::vector<int> const &castArray = static_cast<std::vector<int> >(array);
-	for (std::vector<int>::const_iterator it = castArray.begin(); it != castArray.end(); ++it) {
-		if (*it == index)
-			return it - castArray.begin(); /* Return iterator<->origin adresses' gap */
-	}
-	throw std::exception();
+int	easyfind(T const & array, int value) {
+	std::vector<int>::const_iterator it = std::find(array.begin(), array.end(), value);
+	
+	if (it == array.end())
+		throw std::exception();
+
+	return it - array.begin(); /* Return iterator<->origin adresses' gap */
 }
 
 #endif
